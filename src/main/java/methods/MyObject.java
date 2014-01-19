1b7c232fb97f800845a46933039928f3d66e9cd0
dcrespo <davidcrespoarroyo@gmail.com>
2014-01-19 22:24:22 +0000

    new Kata. Playing with methods and parameters in Java

---
+++ src/main/java/methods/MyObject.java
@@ -0,0 +1,17 @@
+package methods;
+
+/**
+ * Created by dcrespo on 19/01/14.
+ */
+public class MyObject {
+
+    String cad = "test";
+
+    public String getCad() {
+        return cad;
+    }
+
+    public void setCad(String cad) {
+        this.cad = cad;
+    }
+}
