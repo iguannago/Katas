27c0d22375cd1e566f956f0fb6016ba42537a8bd
David Crespo <davidcrespoarroyo@gmail.com>
2015-12-29 14:02:30 +0000

    junit test when amount between 1001 and 2000 both included then returns 1.15% using a  parametrised junit test

--- src/main/java/com/katas/bankAccountInterest/BankAccountInterest6.java
+++ src/main/java/com/katas/bankAccountInterest/BankAccountInterest6.java
@@ -6,7 +6,9 @@ import java.math.BigDecimal;
  * Created by davicres on 29/12/2015.
  */
 public class BankAccountInterest6 {
-    public BigDecimal getRightInterest(int i) {
+    public BigDecimal getRightInterest(int interest) {
+        if (interest > 1000)
+            return new BigDecimal("1.15");
         return new BigDecimal("1.1");
     }
 }
--- src/test/java/com/katas/bankAccountInterest/BankAccountInterestTest6.java
+++ src/test/java/com/katas/bankAccountInterest/BankAccountInterestTest6.java
@@ -2,10 +2,13 @@ package com.katas.bankAccountInterest;
 
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.math.BigDecimal;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by davicres on 29/12/2015.
@@ -13,16 +16,35 @@ import java.math.BigDecimal;
 @RunWith(JUnitParamsRunner.class)
 public class BankAccountInterestTest6 {
 
+    private BankAccountInterest6 bankAccountInterest6;
+
+    @Before
+    public void setUp() throws Exception {
+        bankAccountInterest6 = new BankAccountInterest6();
+    }
+
     @Test
     @Parameters(method = "getAmountBetween1And1000BothIncluded")
     public void givenAnAmountBetween1And1000BothIncludedThenReturns1_1InterestTest(int amountBetween1And1000) {
-        BankAccountInterest6 bankAccountInterest6 = new BankAccountInterest6();
-        BigDecimal interest = bankAccountInterest6.getRightInterest(amountBetween1And1000);
+        assertEquals(new BigDecimal("1.1"), bankAccountInterest6.getRightInterest(amountBetween1And1000));
     }
 
     private static final Object[] getAmountBetween1And1000BothIncluded() {
         return new Integer[][]{
                 {1}, {234}, {500}, {1000}
+        };
+    }
+
+    @Test
+    @Parameters(method = "amountBetween1001And2000")
+    public void givenAnAmountBetween1001And2000BothIncludedThenReturns1_15InterestTest(int amountBetween1001And2000) {
+        assertEquals(new BigDecimal("1.15"), bankAccountInterest6.getRightInterest(amountBetween1001And2000));
+
+    }
+
+    private static final Object[] amountBetween1001And2000() {
+        return new Integer[][]{
+                {1001}, {1120}, {2000}
         };
     }
 }
