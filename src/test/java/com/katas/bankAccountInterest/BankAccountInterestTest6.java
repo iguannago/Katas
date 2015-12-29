1597e02eb9ef779c5de329185a5b3e780413d883
David Crespo <davidcrespoarroyo@gmail.com>
2015-12-29 13:54:23 +0000

    junit test when amount between 1 and 1000 both included then returns 1.1% using a  parametrised junit test

--- src/test/java/com/katas/bankAccountInterest/BankAccountInterestTest6.java
+++ src/test/java/com/katas/bankAccountInterest/BankAccountInterestTest6.java
@@ -1,20 +1,28 @@
 package com.katas.bankAccountInterest;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.math.BigDecimal;
-
-import static org.junit.Assert.assertEquals;
 
 /**
  * Created by davicres on 29/12/2015.
  */
+@RunWith(JUnitParamsRunner.class)
 public class BankAccountInterestTest6 {
 
     @Test
-    public void given1ThenReturns1_1() {
+    @Parameters(method = "getAmountBetween1And1000BothIncluded")
+    public void givenAnAmountBetween1And1000BothIncludedThenReturns1_1InterestTest(int amountBetween1And1000) {
         BankAccountInterest6 bankAccountInterest6 = new BankAccountInterest6();
-        BigDecimal interest = bankAccountInterest6.getRightInterest(1);
-        assertEquals(new BigDecimal("1.1"), interest);
+        BigDecimal interest = bankAccountInterest6.getRightInterest(amountBetween1And1000);
+    }
+
+    private static final Object[] getAmountBetween1And1000BothIncluded() {
+        return new Integer[][]{
+                {1}, {234}, {500}, {1000}
+        };
     }
 }
