45bb769c4aa846e5f364c23f5e23fe114951cefb
David Crespo <davidcrespoarroyo@gmail.com>
2016-03-17 16:39:56 +0000

    Refactored test code to use JUnitParamsRunner to parametrised the different contigous number for the test and to obey with the DRY principle.

--- DavidCrespoArroyo-Step1/src/test/java/com/getinthecloud/programming/kata/FizzBuzzStep1Test.java
+++ DavidCrespoArroyo-Step1/src/test/java/com/getinthecloud/programming/kata/FizzBuzzStep1Test.java
@@ -1,25 +1,32 @@
 package com.getinthecloud.programming.kata;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by davicres on 17/03/2016.
  */
+@RunWith(JUnitParamsRunner.class)
 public class FizzBuzzStep1Test {
 
     private final FizzBuzzStep1 fizzBuzzStep1 = new FizzBuzzStep1();
 
     @Test
-    public void when1Prints1() {
-        assertEquals("1", fizzBuzzStep1.printFizzBuzz(1));
+    @Parameters(method = "getContiguousNumber")
+    public void whenNumberPrintsFizzBuzzOrNumber(int contiguousNumber) {
+        assertEquals(String.valueOf(contiguousNumber), fizzBuzzStep1.printFizzBuzz(contiguousNumber));
     }
 
-    @Test
-    public void when2Prints2() {
-        assertEquals("2", fizzBuzzStep1.printFizzBuzz(2));
+    private static final Object[] getContiguousNumber() {
+        return new Integer[][]{
+                {1}, {2}
+        };
     }
+
 
 
 
