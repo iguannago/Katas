98052681fc1b4d223aeea44ade8d890230d1da0d
David Crespo <davidcrespoarroyo@gmail.com>
2016-04-08 15:38:29 +0100

    Refactored the test code to create apples and oranges on demand.

--- capgemini/src/test/java/com.capgemini.shopcart/CheckoutSystemTest.java
+++ capgemini/src/test/java/com.capgemini.shopcart/CheckoutSystemTest.java
@@ -3,6 +3,7 @@ package com.capgemini.shopcart;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,15 +16,32 @@ public class CheckoutSystemTest {
     public static final BigDecimal APPLE_COST = new BigDecimal("0.60");
     public static final BigDecimal ORANGE_COST = new BigDecimal("0.25");
     private CheckoutSystem checkoutSystem = new CheckoutSystem();
-    private List<Item> items;
+    private List<Item> items = new ArrayList<Item>();
 
     @Test
     public void totalCostTestForAnAppleAndAnOrange() {
-        Item apple = new Apple(APPLE_COST);
-        Item orange = new Orange(ORANGE_COST);
-        items = Arrays.asList(apple, orange);
-        BigDecimal expected = apple.getCost().add(orange.getCost());
-        assertEquals(expected, checkoutSystem.totalCost(items));
+        createGivenNumberOfApplesAndOrangesAndAddThemToItems(1,1);
+        assertEquals(getExpectedResult(), checkoutSystem.totalCost(items));
+    }
+
+    private BigDecimal getExpectedResult() {
+        BigDecimal expected = new BigDecimal("0.0");
+        for (Item item: items) {
+            expected = expected.add(item.getCost());
+        }
+        return expected;
+    }
+
+    public void createGivenNumberOfApplesAndOrangesAndAddThemToItems(int numberOfApples, int numberOfOrange){
+        Item item = null;
+        for (int i = 0; i < numberOfApples; i++) {
+            item = new Apple(APPLE_COST);
+            items.add(item);
+        }
+        for (int i = 0; i < numberOfOrange; i++) {
+            item = new Orange(ORANGE_COST);
+            items.add(item);
+        }
     }
 
     @Test
