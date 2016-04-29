96e214c9f4e3e9646503a7f664ca4a81ed606f3a
David Crespo <davidcrespoarroyo@gmail.com>
2016-04-29 14:24:16 +0100

    Added CounterStringTest and first case scneario test. Added production code to pass the test. Refactored code.

---
+++ src/main/java/com/katas/CounterString/CounterString.java
@@ -0,0 +1,38 @@
+package com.katas.CounterString;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by davicres on 29/04/2016.
+ */
+public class CounterString {
+    public List<Integer> count(String cad) {
+        List<Integer> result = new ArrayList<>();
+        Set<Character> uniqueChars = getUniqueCharsFromString(cad);
+        for (char c: uniqueChars){
+            result.add(countGivenCharInCad(cad, c));
+        }
+        return result;
+    }
+
+    private Set<Character> getUniqueCharsFromString(String cad) {
+        Set<Character> chars = new HashSet<>();
+        for (int i = 0; i < cad.length(); i++) {
+            chars.add(cad.charAt(i));
+        }
+        return chars;
+    }
+
+    private int countGivenCharInCad(String cad, char c) {
+        int counter = 0;
+        for (int i = 0; i < cad.length(); i++) {
+            if (cad.charAt(i) == c) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
---
+++ src/test/java/com/katas/StringCounter/StringCounterTest.java
@@ -0,0 +1,27 @@
+package com.katas.StringCounter;
+
+import com.katas.CounterString.CounterString;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by davicres on 29/04/2016.
+ */
+public class StringCounterTest {
+    @Test
+    public void countLetterAInString(){
+        String cad = "aabcabzc";
+        System.out.println("cad: " + cad);
+        CounterString counterString = new CounterString();
+        List<Integer> result = counterString.count(cad);
+        System.out.println("result: " + result);
+        List<Integer> expected = new ArrayList<>(Arrays.asList(3,2,2,1));
+        System.out.printf("expected: " + expected);
+        assertEquals(expected, result);
+    }
+}
