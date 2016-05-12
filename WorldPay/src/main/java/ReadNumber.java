78483f4da784741843d59e492bb922df2b12fa3b
David Crespo <davidcrespoarroyo@gmail.com>
2016-05-12 20:11:13 +0100

    Refactored production code

--- WorldPay/src/main/java/ReadNumber.java
+++ WorldPay/src/main/java/ReadNumber.java
@@ -6,38 +6,40 @@ import java.util.List;
  */
 public class ReadNumber {
 
+    private final List<String> cadUpTo19 = Arrays.asList("zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+            "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+            "eighteen", "nineteen");
+    private final List<String> cadFrom20 = Arrays.asList("twenty", "thirty", "forty");
+
     public String read(int number) {
         System.out.println("number: " + number);
-        List<String> cad = Arrays.asList("zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
-                "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
-                "eighteen", "nineteen");
-        String result = "";
+
+        String result;
 
         if (number >= 30) {
-            int mod = number % 30;
-            if (mod == 0) {
-                result = "thirty";
-            }
-            else {
-                result =  "thirty " + cad.get(number % 30);
-            }
-            System.out.println("result: " + result);
+            result = getString(number, 30, 1);
             return result;
         }
 
         if (number >= 20) {
-            int mod = number % 20;
-            if (mod == 0) {
-                result = "twenty";
-            }
-            else {
-                result = "twenty " + cad.get(number % 20);
-            }
-            System.out.println("result: " + result);
+            result = getString(number, 20, 0);
             return result;
         }
 
-        result = cad.get(number);
+        result = cadUpTo19.get(number);
+        System.out.println("result: " + result);
+        return result;
+    }
+
+    private String getString(int number, int range, int index) {
+        String result;
+        int mod = number % range;
+        if (mod == 0) {
+            result = cadFrom20.get(index);
+        }
+        else {
+            result =  cadFrom20.get(index) + " " + cadUpTo19.get(number % range);
+        }
         System.out.println("result: " + result);
         return result;
     }
