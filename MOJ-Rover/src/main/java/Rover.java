041417c748c8a9d12547f0b49b3c93f2f8c7d9db
David Crespo <davidcrespoarroyo@gmail.com>
2016-05-17 12:08:17 +0100

    Refactored code. Added Production code to pass test.

--- MOJ-Rover/src/main/java/Rover.java
+++ MOJ-Rover/src/main/java/Rover.java
@@ -3,26 +3,26 @@
  */
 public final class Rover {
     private final Position position;
-    private final Instruction instruction;
+    private final String instruction;
 
-    private Rover(Position position, Instruction instruction) {
+    private Rover(Position position, String instruction) {
         this.position = position;
         this.instruction = instruction;
     }
 
-    public static Rover create(Position position, Instruction instruction) {
+    public static Rover create(Position position, String instruction) {
         return new Rover(position, instruction);
     }
 
     public Position move() {
-        return Position.create(0,0,'R');
+        return Position.create(0,0, instruction.charAt(instruction.length()-1));
     }
 
     @Override
     public String toString() {
         return "Rover{" +
                 "position=" + position +
-                ", instruction=" + instruction +
+                ", instruction='" + instruction + '\'' +
                 '}';
     }
 }
--- MOJ-Rover/src/test/java/RoverTest.java
+++ MOJ-Rover/src/test/java/RoverTest.java
@@ -7,12 +7,25 @@ import static org.junit.Assert.assertEquals;
  * Created by davicres on 17/05/2016.
  */
 public class RoverTest {
+
+    private final Position position = Position.create(0,0,'N');
+
+
     @Test
-    public void roverMoveTest() {
-        Position position = Position.create(0,0,'N');
-        Instruction instruction = Instruction.create("R");
-        Rover rover = Rover.create(position, instruction);
+    public void roverMoveToTheRightTest() {
+        Rover rover = Rover.create(position, "R");
         //TODO: improve the object assertion - how to compare objects.
         assertEquals(Position.create(0,0,'R').toString(), rover.move().toString());
     }
+
+    @Test
+    public void roverMoveToTheLeftTest() {
+        Rover rover = Rover.create(position, "L");
+        //TODO: improve the object assertion - how to compare objects.
+        assertEquals(Position.create(0,0,'L').toString(), rover.move().toString());
+    }
+    //TODO: parameterise the above tests to obly with the DRY.
+
+
+
 }
