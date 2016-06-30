135f886f8b44be00763fc8aa1e9ecaccbc15fd4a
David Crespo <davidcrespoarroyo@gmail.com>
2016-06-30 13:10:08 +0100

    refactored testing code.

--- src/test/java/com/katas/fizzbuzz/FizzBuzzTest18.java
+++ src/test/java/com/katas/fizzbuzz/FizzBuzzTest18.java
@@ -8,18 +8,17 @@ import static org.junit.Assert.assertEquals;
  * Created by davicres on 30/06/2016.
  */
 public class FizzBuzzTest18 {
+
+    private final FizzBuzz18 fizzBuzz18 = new FizzBuzz18();
+
     @Test
     public void when1Prints1Test() {
-        FizzBuzz18 fizzBuzz18 = new FizzBuzz18();
-        String result = fizzBuzz18.print(1);
-        assertEquals("1", result);
+        assertEquals("1", fizzBuzz18.print(1));
     }
 
     @Test
     public void when2Prints2Test() {
-        FizzBuzz18 fizzBuzz18 = new FizzBuzz18();
-        String result = fizzBuzz18.print(2);
-        assertEquals("2", result);
+        assertEquals("2", fizzBuzz18.print(2));
     }
 
 }
