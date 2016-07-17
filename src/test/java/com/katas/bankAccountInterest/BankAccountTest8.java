14f9988e5ce6c38cd23bba83c44e05c5b5086507
David Crespo <davidcrespoarroyo@gmail.com>
2016-07-17 18:56:54 +0100

    Added more tests to tes the  1.1 interest scenario.

--- src/test/java/com/katas/bankAccountInterest/BankAccountTest8.java
+++ src/test/java/com/katas/bankAccountInterest/BankAccountTest8.java
@@ -10,10 +10,24 @@ import static org.junit.Assert.assertEquals;
  * Created by davicres on 17/07/2016.
  */
 public class BankAccountTest8 {
+
+    private final BankAccount8 bankAccount8 = new BankAccount8();
+
     @Test
     public void given1Return1_1() {
-        BankAccount8 bankAccount8 = new BankAccount8();
         assertEquals(bankAccount8.getInterest(1), new BigDecimal("1.1"));
     }
+
+    @Test
+    public void given500Return1_1() {
+        assertEquals(bankAccount8.getInterest(500), new BigDecimal("1.1"));
+    }
+
+    @Test
+    public void given1000Return1_1() {
+        assertEquals(bankAccount8.getInterest(1000), new BigDecimal("1.1"));
+    }
+
+
 
 }
