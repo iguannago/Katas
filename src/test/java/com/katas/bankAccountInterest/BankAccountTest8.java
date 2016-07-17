e25b9db31498332d2ddd14664627540100474ecb
David Crespo <davidcrespoarroyo@gmail.com>
2016-07-17 19:02:01 +0100

    Refactored test code to parameterise the tests and follow the DRY principle.

--- src/test/java/com/katas/bankAccountInterest/BankAccountTest8.java
+++ src/test/java/com/katas/bankAccountInterest/BankAccountTest8.java
@@ -1,33 +1,33 @@
 package com.katas.bankAccountInterest;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.math.BigDecimal;
 
+import static junitparams.JUnitParamsRunner.$;
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by davicres on 17/07/2016.
  */
+@RunWith(JUnitParamsRunner.class)
 public class BankAccountTest8 {
 
     private final BankAccount8 bankAccount8 = new BankAccount8();
 
     @Test
-    public void given1Return1_1() {
-        assertEquals(bankAccount8.getInterest(1), new BigDecimal("1.1"));
+    @Parameters(method = "getAmountOfMoney")
+    public void given1Return1_1(int money) {
+        assertEquals(bankAccount8.getInterest(money), new BigDecimal("1.1"));
     }
 
-    @Test
-    public void given500Return1_1() {
-        assertEquals(bankAccount8.getInterest(500), new BigDecimal("1.1"));
+    private Object[] getAmountOfMoney() {
+        return $(
+                $(1), $(500), $(1000)
+        );
     }
-
-    @Test
-    public void given1000Return1_1() {
-        assertEquals(bankAccount8.getInterest(1000), new BigDecimal("1.1"));
-    }
-
-
 
 }
