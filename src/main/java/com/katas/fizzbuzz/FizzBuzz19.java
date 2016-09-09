ec306643c2a25422f185c4e75763015ae9925e9f
David Crespo <davidcrespoarroyo@gmail.com>
2016-09-09 13:12:34 +0100

    Added simple first test case.

---
+++ src/main/java/com/katas/fizzbuzz/FizzBuzz19.java
@@ -0,0 +1,10 @@
+package com.katas.fizzbuzz;
+
+/**
+ * Created by davicres on 09/09/2016.
+ */
+public class FizzBuzz19 {
+    public String print(int i) {
+        return "1";
+    }
+}
---
+++ src/test/java/com/katas/fizzbuzz/FizzBuzzTest19.java
@@ -0,0 +1,17 @@
+package com.katas.fizzbuzz;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by davicres on 09/09/2016.
+ */
+public class FizzBuzzTest19 {
+
+    @Test
+    public void printTest() {
+        FizzBuzz19 fizzBuzz19 = new FizzBuzz19();
+        assertEquals("1", fizzBuzz19.print(1));
+    }
+}
