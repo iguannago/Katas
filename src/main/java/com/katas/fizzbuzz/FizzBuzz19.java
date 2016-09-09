641740a2b5e1f1b83437ce7d36bf9f9ad46d256c
David Crespo <davidcrespoarroyo@gmail.com>
2016-09-09 13:14:52 +0100

    Added second junit test and refactored code

--- src/main/java/com/katas/fizzbuzz/FizzBuzz19.java
+++ src/main/java/com/katas/fizzbuzz/FizzBuzz19.java
@@ -4,7 +4,7 @@ package com.katas.fizzbuzz;
  * Created by davicres on 09/09/2016.
  */
 public class FizzBuzz19 {
-    public String print(int i) {
-        return "1";
+    public String print(int number) {
+        return String.valueOf(number);
     }
 }
--- src/test/java/com/katas/fizzbuzz/FizzBuzzTest19.java
+++ src/test/java/com/katas/fizzbuzz/FizzBuzzTest19.java
@@ -10,8 +10,16 @@ import static org.junit.Assert.assertEquals;
 public class FizzBuzzTest19 {
 
     @Test
-    public void printTest() {
+    public void print1When1Test() {
         FizzBuzz19 fizzBuzz19 = new FizzBuzz19();
         assertEquals("1", fizzBuzz19.print(1));
     }
+
+    @Test
+    public void print2When2Test() {
+        FizzBuzz19 fizzBuzz19 = new FizzBuzz19();
+        assertEquals("2", fizzBuzz19.print(2));
+    }
+
+
 }
