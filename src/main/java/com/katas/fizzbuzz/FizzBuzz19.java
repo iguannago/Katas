8eb4f0cd1391f567054c4f4464521defe4b91347
David Crespo <davidcrespoarroyo@gmail.com>
2016-09-09 13:40:32 +0100

    Added more test cases to cover the FizzBuzz scenario.

--- src/main/java/com/katas/fizzbuzz/FizzBuzz19.java
+++ src/main/java/com/katas/fizzbuzz/FizzBuzz19.java
@@ -5,13 +5,20 @@ package com.katas.fizzbuzz;
  */
 public class FizzBuzz19 {
     public String print(int number) {
+        if (isMultipleOf3(number)&&isMultipleOf5(number)) {
+            return "FizzBuzz";
+        }
         if (isMultipleOf3(number)) {
             return "Fizz";
         }
-        if (number == 5) {
+        if (isMultipleOf5(number)) {
             return "Buzz";
         }
         return String.valueOf(number);
+    }
+
+    private boolean isMultipleOf5(int number) {
+        return number % 5 == 0;
     }
 
     private boolean isMultipleOf3(int number) {
--- src/test/java/com/katas/fizzbuzz/FizzBuzzTest19.java
+++ src/test/java/com/katas/fizzbuzz/FizzBuzzTest19.java
@@ -13,10 +13,12 @@ import static org.junit.Assert.assertEquals;
 @RunWith(JUnitParamsRunner.class)
 public class FizzBuzzTest19 {
 
+    private final FizzBuzz19 fizzBuzz19 = new FizzBuzz19();
+
     @Test
-    @Parameters({"1, 1", "2, 2", "Fizz, 3", "Buzz, 5", "Fizz, 6", "7, 7"})
+    @Parameters({"1, 1", "2, 2", "Fizz, 3", "Buzz, 5", "Fizz, 6", "7, 7", "Fizz, 9", "Buzz, 10", "Fizz, 12",
+            "FizzBuzz, 15", "FizzBuzz, 30"})
     public void print1When1Test(String expected, int number) {
-        FizzBuzz19 fizzBuzz19 = new FizzBuzz19();
         assertEquals(expected, fizzBuzz19.print(number));
     }
 
