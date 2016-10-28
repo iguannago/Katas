7bea387860c711fcf6d0036dd194406c103b60cf
David Crespo <davidcrespoarroyo@gmail.com>
2016-10-28 18:54:47 +0100

    Added more test case scenarios and production code to pass them.

--- src/main/java/com/katas/fizzbuzz/FizzBuzz20.java
+++ src/main/java/com/katas/fizzbuzz/FizzBuzz20.java
@@ -4,7 +4,24 @@ package com.katas.fizzbuzz;
  * Created by davicres on 28/10/2016.
  */
 public class FizzBuzz20 {
-    public String print(int i) {
-        return String.valueOf(i);
+    public String print(int input) {
+        if (isMultipleOf3(input)&&isMultipleOf5(input)) {
+            return "FizzBuzz";
+        }
+        if (isMultipleOf3(input)) {
+            return "Fizz";
+        }
+        if (isMultipleOf5(input)) {
+            return "Buzz";
+        }
+        return String.valueOf(input);
+    }
+
+    private boolean isMultipleOf5(int input) {
+        return input % 5 == 0;
+    }
+
+    private boolean isMultipleOf3(int input) {
+        return input % 3 == 0;
     }
 }
--- src/test/java/com/katas/fizzbuzz/FizzBuzzTest20.java
+++ src/test/java/com/katas/fizzbuzz/FizzBuzzTest20.java
@@ -16,7 +16,8 @@ public class FizzBuzzTest20 {
     private final FizzBuzz20 fizzBuzz20 = new FizzBuzz20();
 
     @Test
-    @Parameters({"1, 1", "2, 2"})
+    @Parameters({"1, 1", "2, 2", "Fizz, 3", "4, 4", "Buzz, 5", "Fizz, 6", "7, 7", "Fizz, 9", "Buzz, 10",
+    "FizzBuzz, 15", "FizzBuzz, 30"})
     public void printMethodTest(String expected, int input) {
         assertEquals(expected, fizzBuzz20.print(input));
     }
