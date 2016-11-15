f0432375754c95fbb96875d16ec154df15f2117d
David Crespo <davidcrespoarroyo@gmail.com>
2016-11-15 17:49:59 +0000

    Added junit test case when 5 and return Buzz. Added production code.

--- src/main/java/com/katas/fizzbuzz/FizzBuzz21.java
+++ src/main/java/com/katas/fizzbuzz/FizzBuzz21.java
@@ -8,6 +8,9 @@ public class FizzBuzz21 {
         if (number == 3) {
             return "Fizz";
         }
+        if (number == 5) {
+            return "Buzz";
+        }
         return String.valueOf(number);
     }
 }
