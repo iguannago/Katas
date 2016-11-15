648db50328c6a3989808a5c926423a4875ff683e
David Crespo <davidcrespoarroyo@gmail.com>
2016-11-15 17:46:44 +0000

    Added junit test and production code to pass it.

---
+++ src/main/java/com/katas/fizzbuzz/FizzBuzz21.java
@@ -0,0 +1,10 @@
+package com.katas.fizzbuzz;
+
+/**
+ * Created by davicres on 15/11/2016.
+ */
+public class FizzBuzz21 {
+    public String print(int number) {
+        return String.valueOf(number);
+    }
+}
---
+++ src/test/java/com/katas/fizzbuzz/FizzBuzzTest21.java
@@ -0,0 +1,23 @@
+package com.katas.fizzbuzz;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by davicres on 15/11/2016.
+ */
+@RunWith(JUnitParamsRunner.class)
+public class FizzBuzzTest21 {
+
+    @Test
+    @Parameters({"1, 1"})
+    public void when1Prints1Test(int input, String expected) {
+        FizzBuzz21 fizzBuzz21 = new FizzBuzz21();
+        String actual = fizzBuzz21.print(input);
+        assertEquals(expected, actual);
+    }
+}
