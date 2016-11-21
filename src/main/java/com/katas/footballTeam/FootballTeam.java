f4224142e1367dd922258d983957abb2fea16b34
David Crespo <davidcrespoarroyo@gmail.com>
2016-11-21 14:38:49 +0000

    Added main method to create a little app pretending to be the premier league.

--- src/main/java/com/katas/footballTeam/FootballTeam.java
+++ src/main/java/com/katas/footballTeam/FootballTeam.java
@@ -1,5 +1,9 @@
 package com.katas.footballTeam;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by davicres on 21/11/2016.
  */
@@ -22,6 +26,26 @@ public final class FootballTeam implements Comparable<FootballTeam> {
 
     public int getGamesWon() {
         return gamesWon;
+    }
+
+    @Override
+    public String toString() {
+        return "FootballTeam{" +
+                "gamesWon=" + gamesWon +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        FootballTeam chelsea = FootballTeam.builder(5);
+        FootballTeam city = FootballTeam.builder(3);
+        FootballTeam united = FootballTeam.builder(1);
+        List<FootballTeam> premier = new ArrayList<>(3);
+        premier.add(city);
+        premier.add(united);
+        premier.add(chelsea);
+        System.out.println("before soring: " + premier);
+        Collections.sort(premier);
+        System.out.println("after soring: " + premier);
     }
 
 }
