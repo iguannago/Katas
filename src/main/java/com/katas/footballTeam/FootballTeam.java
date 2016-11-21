e39f27a76ea1ee32f9bcc39429d95f5d7f32cb2a
David Crespo <davidcrespoarroyo@gmail.com>
2016-11-21 14:04:40 +0000

    Added junit test to sort footballTeams.

--- src/main/java/com/katas/footballTeam/FootballTeam.java
+++ src/main/java/com/katas/footballTeam/FootballTeam.java
@@ -3,8 +3,13 @@ package com.katas.footballTeam;
 /**
  * Created by davicres on 21/11/2016.
  */
-public class FootballTeam {
+public class FootballTeam implements Comparable<FootballTeam> {
     public FootballTeam(int gamesWon) {
 
+    }
+
+    @Override
+    public int compareTo(FootballTeam o) {
+        return 0;
     }
 }
--- src/test/java/com/katas/footballTeam/FootballTeamTest.java
+++ src/test/java/com/katas/footballTeam/FootballTeamTest.java
@@ -2,6 +2,10 @@ package com.katas.footballTeam;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.Assert.assertNotNull;
 
 /**
@@ -13,6 +17,12 @@ public class FootballTeamTest {
     public void constructorTest() {
         FootballTeam footballTeam = new FootballTeam(2);
         assertNotNull(footballTeam);
+    }
+
+    @Test
+    public void sortFootballTeamsTest() {
+        List<FootballTeam> footballTeamList = new ArrayList<>();
+        Collections.sort(footballTeamList);
     }
 
 }
