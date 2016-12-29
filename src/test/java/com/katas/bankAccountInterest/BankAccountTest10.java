a1605f1b932615cbd1c125379d9af3b679476fb4
David Crespo <david.crespo@hmcts.net>
2016-12-29 20:18:12 +0000

    Add test case when amount is 2.

--- src/test/java/com/katas/bankAccountInterest/BankAccountTest10.java
+++ src/test/java/com/katas/bankAccountInterest/BankAccountTest10.java
@@ -10,10 +10,18 @@ import static org.junit.Assert.assertEquals;
  * Created by davicres on 29/12/2016.
  */
 public class BankAccountTest10 {
+
+    private final BankAccount10 bankAccount10 = new BankAccount10();
+
     @Test
     public void whenAmountIs1ThenInterestIs1_1() throws Exception {
-        BankAccount10 bankAccount10 = new BankAccount10();
-        BigDecimal interest = bankAccount10.getInterest(1);
-        assertEquals(new BigDecimal("1.1"), interest);
+        assertEquals(new BigDecimal("1.1"), bankAccount10.getInterest(1));
     }
+
+    @Test
+    public void whenAmountIs2ThenInterestIs1_1() throws Exception {
+        assertEquals(new BigDecimal("1.1"), bankAccount10.getInterest(2));
+    }
+
+
 }
