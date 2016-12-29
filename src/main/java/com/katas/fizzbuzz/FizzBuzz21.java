83e04ca5aadcaaec31766611308b86cbab6a74ae
David Crespo <david.crespo@hmcts.net>
2016-12-29 10:38:31 +0000

    Add Calendar in a static statement. Add message when throwing exception.

--- src/main/java/com/katas/fizzbuzz/FizzBuzz21.java
+++ src/main/java/com/katas/fizzbuzz/FizzBuzz21.java
@@ -1,12 +1,20 @@
 package com.katas.fizzbuzz;
 
+import java.util.Calendar;
+
 /**
  * Created by davicres on 15/11/2016.
  */
 public class FizzBuzz21 {
+
+    static {
+        Calendar cal = Calendar.getInstance();
+        System.out.println("cal: " + cal.getTime());
+    }
+
     public String print(int number) {
         if (number < 1 || number > 100) {
-            throw new RuntimeException();
+            throw new RuntimeException("Number is invalid");
         }
         if (isMultipleOf3(number) && isMultipleOf5(number)) {
             return "FizzBuzz";
@@ -26,5 +34,12 @@ public class FizzBuzz21 {
 
     private boolean isMultipleOf3(int number) {
         return number % 3 == 0;
+    }
+
+    public static void main(String[] args) {
+        FizzBuzz21 fizzBuzz21 = new FizzBuzz21();
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(fizzBuzz21.print(i));
+        }
     }
 }
