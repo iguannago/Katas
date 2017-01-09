9996223162088b02e743e67021493e3618586d20
David Crespo <david.crespo@hmcts.net>
2017-01-09 20:16:32 +0000

    Refactor testing code.

--- src/test/java/com/katas/footballTeam/FootballTeamTest1.java
+++ src/test/java/com/katas/footballTeam/FootballTeamTest1.java
@@ -2,14 +2,14 @@ package com.katas.footballTeam;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by davicres on 09/01/2017.
  */
 public class FootballTeamTest1 {
     @Test
     public void giveMeNumberOfGamesWon() throws Exception {
-        FootballTeam1 footballTeam1 = new FootballTeam1(1);
-        org.junit.Assert.assertEquals(1, footballTeam1.getNumberOfGamesWon());
-
+        assertEquals(1, new FootballTeam1(1).getNumberOfGamesWon());
     }
 }
