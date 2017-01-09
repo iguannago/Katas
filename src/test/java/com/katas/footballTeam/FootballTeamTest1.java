5d7adadff7ab101ad70b413bce19694c01a083b9
David Crespo <david.crespo@hmcts.net>
2017-01-09 22:07:00 +0000

    Refactor testing code: parametrise tests. Add more tests

--- src/test/java/com/katas/footballTeam/FootballTeamTest1.java
+++ src/test/java/com/katas/footballTeam/FootballTeamTest1.java
@@ -1,12 +1,16 @@
 package com.katas.footballTeam;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by davicres on 09/01/2017.
  */
+@RunWith(JUnitParamsRunner.class)
 public class FootballTeamTest1 {
     @Test
     public void giveMeNumberOfGamesWon() throws Exception {
@@ -14,23 +18,25 @@ public class FootballTeamTest1 {
     }
 
     @Test
-    public void footballTeamWith1GameWonRankHigherThanFootballTeamWith0GamesWon() throws Exception {
-        FootballTeam1 footballTeam1With1GameWon = FootballTeam1.build(1);
-        FootballTeam1 footballTeam1With0GamesWon = FootballTeam1.build(0);
-        assertEquals(true, footballTeam1With1GameWon.compareTo(footballTeam1With0GamesWon) == 1);
+    @Parameters({"1, 0", "4, 0"})
+    public void footballTeamWith1GameWonRankHigherThanFootballTeamWith0GamesWon(int winnerGames, int loserGames)
+            throws Exception {
+        FootballTeam1 teamWinner = FootballTeam1.build(winnerGames);
+        FootballTeam1 teamLoser = FootballTeam1.build(loserGames);
+        assertEquals(true, teamWinner.compareTo(teamLoser) > 0);
     }
 
     @Test
     public void teamWithFewerGamesWonRankLowerThanTeamWithGreaterGamesWon() throws Exception {
-        FootballTeam1 footballTeam1With1GameWon = FootballTeam1.build(1);
-        FootballTeam1 footballTeam1With0GamesWon = FootballTeam1.build(0);
-        assertEquals(true, footballTeam1With0GamesWon.compareTo(footballTeam1With1GameWon) < 0);
+        FootballTeam1 teamWinner = FootballTeam1.build(1);
+        FootballTeam1 teamLoser = FootballTeam1.build(0);
+        assertEquals(true, teamLoser.compareTo(teamWinner) < 0);
     }
 
     @Test
     public void teamWithEqualGamesWonRankEqually() throws Exception {
-        FootballTeam1 footballTeam1With1GameWon = FootballTeam1.build(1);
-        FootballTeam1 footballTeam1With0GamesWon = FootballTeam1.build(1);
-        assertEquals(true, footballTeam1With0GamesWon.compareTo(footballTeam1With1GameWon) == 0);
+        FootballTeam1 teamWinner = FootballTeam1.build(1);
+        FootballTeam1 teamLoser = FootballTeam1.build(1);
+        assertEquals(true, teamLoser.compareTo(teamWinner) == 0);
     }
 }
