210b591a873c337147134f9111b82edbb1541d53
David Crespo <david.crespo@hmcts.net>
2017-01-09 19:39:39 +0000

    Add first test. Add production code to pass it.

---
+++ src/main/java/com/katas/fizzbuzz/FizzBuzz22.java
@@ -0,0 +1,10 @@
+package com.katas.fizzbuzz;
+
+/**
+ * Created by davicres on 09/01/2017.
+ */
+public class FizzBuzz22 {
+    public String print(int number) {
+        return String.valueOf(number);
+    }
+}
---
+++ src/test/java/com/katas/fizzbuzz/FizzBuzzTest22.java
@@ -0,0 +1,16 @@
+package com.katas.fizzbuzz;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by davicres on 09/01/2017.
+ */
+public class FizzBuzzTest22 {
+    @Test
+    public void when1Print1() throws Exception {
+        FizzBuzz22 fizzBuzz22 = new FizzBuzz22();
+        assertEquals("1", fizzBuzz22.print(1));
+    }
+}
