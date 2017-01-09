1b18b0ba651aa357e6275ad526f94ed70a8255fb
David Crespo <david.crespo@hmcts.net>
2017-01-09 19:43:25 +0000

    Refactor testing code. Reuse variable making it field.

--- src/test/java/com/katas/fizzbuzz/FizzBuzzTest22.java
+++ src/test/java/com/katas/fizzbuzz/FizzBuzzTest22.java
@@ -8,15 +8,16 @@ import static org.junit.Assert.assertEquals;
  * Created by davicres on 09/01/2017.
  */
 public class FizzBuzzTest22 {
+
+    private final FizzBuzz22 fizzBuzz22 = new FizzBuzz22();
+
     @Test
     public void when1Print1() throws Exception {
-        FizzBuzz22 fizzBuzz22 = new FizzBuzz22();
         assertEquals("1", fizzBuzz22.print(1));
     }
 
     @Test
     public void when3PrintFizz() throws Exception {
-        FizzBuzz22 fizzBuzz22 = new FizzBuzz22();
         assertEquals("Fizz", fizzBuzz22.print(3));
     }
 }
