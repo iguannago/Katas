ae01d65b484983ca647fcc64ed144441ef68586a
David Crespo <david.crespo@hmcts.net>
2017-01-09 19:46:15 +0000

    Refactor testing code. Parametrise tests.

--- src/test/java/com/katas/fizzbuzz/FizzBuzzTest22.java
+++ src/test/java/com/katas/fizzbuzz/FizzBuzzTest22.java
@@ -1,23 +1,24 @@
 package com.katas.fizzbuzz;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by davicres on 09/01/2017.
  */
+@RunWith(JUnitParamsRunner.class)
 public class FizzBuzzTest22 {
 
     private final FizzBuzz22 fizzBuzz22 = new FizzBuzz22();
 
     @Test
-    public void when1Print1() throws Exception {
-        assertEquals("1", fizzBuzz22.print(1));
+    @Parameters({"1, 1", "3, Fizz"})
+    public void when1Print1(int input, String expected) throws Exception {
+        assertEquals(expected, fizzBuzz22.print(input));
     }
 
-    @Test
-    public void when3PrintFizz() throws Exception {
-        assertEquals("Fizz", fizzBuzz22.print(3));
-    }
 }
