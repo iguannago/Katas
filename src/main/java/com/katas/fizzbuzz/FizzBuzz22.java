dcce81b2385777bd020db7eaffb1a1d48646a736
David Crespo <david.crespo@hmcts.net>
2017-01-09 19:51:15 +0000

    Add test case for multiple of 5. Add production code to pass it.

--- src/main/java/com/katas/fizzbuzz/FizzBuzz22.java
+++ src/main/java/com/katas/fizzbuzz/FizzBuzz22.java
@@ -8,7 +8,7 @@ public class FizzBuzz22 {
         if (number % 3 == 0) {
             return "Fizz";
         }
-        if (number == 5) {
+        if (number % 5 == 0) {
             return "Buzz";
         }
         return String.valueOf(number);
--- src/test/java/com/katas/fizzbuzz/FizzBuzzTest22.java
+++ src/test/java/com/katas/fizzbuzz/FizzBuzzTest22.java
@@ -16,7 +16,7 @@ public class FizzBuzzTest22 {
     private final FizzBuzz22 fizzBuzz22 = new FizzBuzz22();
 
     @Test
-    @Parameters({"1, 1", "3, Fizz", "4, 4", "5, Buzz", "6, Fizz", "7, 7", "9, Fizz"})
+    @Parameters({"1, 1", "3, Fizz", "4, 4", "5, Buzz", "6, Fizz", "7, 7", "9, Fizz", "10, Buzz"})
     public void when1Print1(int input, String expected) throws Exception {
         assertEquals(expected, fizzBuzz22.print(input));
     }
