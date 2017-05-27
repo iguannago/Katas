60195c10b85e8b307ca3bd48c96ddd478f10e39e
David Crespo <david.crespo@hmcts.net>
2017-05-27 07:39:30 +0100

    Refactor testing code Parametrised code

--- src/test/java/com/katas/fizzbuzz/FizzBuzzTest23.java
+++ src/test/java/com/katas/fizzbuzz/FizzBuzzTest23.java
@@ -1,33 +1,23 @@
 package com.katas.fizzbuzz;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by davicres on 05/04/2017.
  */
+@RunWith(JUnitParamsRunner.class)
 public class FizzBuzzTest23 {
 
     private FizzBuzz23 fizzBuzz23 = new FizzBuzz23();
 
     @Test
-    public void given1Prints1() throws Exception {
-        assertEquals("1", fizzBuzz23.print(1));
-    }
-
-    @Test
-    public void given2Prints2() throws Exception {
-        assertEquals("2", fizzBuzz23.print(2));
-    }
-
-    @Test
-    public void given3PrintsFizz() throws Exception {
-        assertEquals("Fizz", fizzBuzz23.print(3));
-    }
-
-    @Test
-    public void given5PrintsBuzz() throws Exception {
-        assertEquals("Buzz", fizzBuzz23.print(5));
+    @Parameters({"1,1", "2,2", "Fizz, 3", "4, 4", "Buzz, 5"})
+    public void givenNumberPrintFizzBuzz(String expected, int number) throws Exception {
+        assertEquals(expected, fizzBuzz23.print(number));
     }
 }
