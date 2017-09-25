4590dc5c2f86835f6bea8afc3905001f87fa7593
David Crespo <david.crespo@hmcts.net>
2017-09-25 14:44:46 +0200

    Add test cases for the fourth scenario and corner cases.

--- src/main/java/com/katas/bankAccountInterest/BankAccount11.java
+++ src/main/java/com/katas/bankAccountInterest/BankAccount11.java
@@ -9,6 +9,10 @@ public class BankAccount11 {
     final private int amount;
 
     public BigDecimal getInterest() {
+        if (amount < 1)
+            throw new IllegalArgumentException();
+        if (amount > 3000)
+            return new BigDecimal("1.3");
         if (amount > 2000)
             return new BigDecimal("1.22");
         if (amount >= 1001)
