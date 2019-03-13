0cd979b3a050b1eccab7fc4abc0c2c5122f7082c
david.crespo <david.crespo@hmcts.net>
2019-03-13 23:47:14 +0000

    add scenario 2 and production code to pass it

--- src/main/java/com/katas/fizzbuzz/FizzBuzz24.java
+++ src/main/java/com/katas/fizzbuzz/FizzBuzz24.java
@@ -2,6 +2,6 @@ package com.katas.fizzbuzz;
 
 public class FizzBuzz24 {
     public String print(int number) {
-        return "1";
+        return String.valueOf(number);
     }
 }
--- src/test/java/com/katas/fizzbuzz/FizzBuzzTest24.java
+++ src/test/java/com/katas/fizzbuzz/FizzBuzzTest24.java
@@ -5,9 +5,16 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class FizzBuzzTest24 {
+
+    private final FizzBuzz24 fizzBuzz24 = new FizzBuzz24();
+
     @Test
     public void given1_shouldReturn1() {
-        FizzBuzz24 fizzBuzz24 = new FizzBuzz24();
         assertEquals("1", fizzBuzz24.print(1));
+    }
+
+    @Test
+    public void given2_shouldReturn2() {
+        assertEquals("2", fizzBuzz24.print(2));
     }
 }
