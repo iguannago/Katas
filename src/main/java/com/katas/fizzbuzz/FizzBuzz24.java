63e8213565faa4109d2d7b39b73931679ff0aa27
david.crespo <david.crespo@hmcts.net>
2019-03-14 00:04:41 +0000

    Add FizzBuzz scenarios and production code to pass them

--- src/main/java/com/katas/fizzbuzz/FizzBuzz24.java
+++ src/main/java/com/katas/fizzbuzz/FizzBuzz24.java
@@ -2,6 +2,9 @@ package com.katas.fizzbuzz;
 
 public class FizzBuzz24 {
     public String print(int number) {
+        if (isMultipleOf3(number) && isMultipleOf5(number)) {
+            return "FizzBuzz";
+        }
         if (isMultipleOf3(number)) {
             return "Fizz";
         }
@@ -12,7 +15,7 @@ public class FizzBuzz24 {
     }
 
     private boolean isMultipleOf5(int number) {
-        return number%5==0;
+        return number % 5 == 0;
     }
 
     private boolean isMultipleOf3(int number) {
--- src/test/java/com/katas/fizzbuzz/FizzBuzzTest24.java
+++ src/test/java/com/katas/fizzbuzz/FizzBuzzTest24.java
@@ -13,7 +13,8 @@ public class FizzBuzzTest24 {
     private final FizzBuzz24 fizzBuzz24 = new FizzBuzz24();
 
     @Test
-    @Parameters({"1,1", "2,2", "Fizz,3", "4,4", "Buzz,5", "Fizz,6", "7,7", "Fizz,9", "Buzz,10"})
+    @Parameters({"1,1", "2,2", "Fizz,3", "4,4", "Buzz,5", "Fizz,6", "7,7", "Fizz,9", "Buzz,10", "11,11", "Fizz,12",
+            "FizzBuzz, 15", "FizzBuzz,30"})
     public void given1_shouldReturn1(String expected, int number) {
         assertEquals(expected, fizzBuzz24.print(number));
     }
