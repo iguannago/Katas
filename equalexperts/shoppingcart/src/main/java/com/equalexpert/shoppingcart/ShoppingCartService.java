3bb3ed1bed9f2d4d5f79f3bda04f422aca5e4033
david.crespo <david.crespo@hmcts.net>
2021-12-09 16:08:55 +0000

    add edge case scenarios
    add unit tests
    add production code to pass tests

--- equalexperts/shoppingcart/src/main/java/com/equalexpert/shoppingcart/ShoppingCartService.java
+++ equalexperts/shoppingcart/src/main/java/com/equalexpert/shoppingcart/ShoppingCartService.java
@@ -5,13 +5,22 @@ import java.util.List;
 public class ShoppingCartService {
 
     public ShoppingCart addProduct(ShoppingCart shoppingCart, Product newProduct, int units) {
+        if (shoppingCart == null) {
+            throw new RuntimeException("Error: shopping cart is null");
+        }
+        if (shoppingCart.getProducts() == null) {
+            throw new RuntimeException("Error: product list is null");
+        }
+        if (units <= 0) {
+            throw new RuntimeException("Error: units has to be a positive number");
+        }
         if (units == 1) {
             return addProductOnce(shoppingCart, newProduct);
         }
         return addProduct(addProductOnce(shoppingCart, newProduct), newProduct, --units);
     }
 
-    public ShoppingCart addProductOnce(ShoppingCart shoppingCart, Product newProduct) {
+    private ShoppingCart addProductOnce(ShoppingCart shoppingCart, Product newProduct) {
         List<Product> products = shoppingCart.getProducts();
         products.add(newProduct);
         return new ShoppingCart(products, shoppingCart.getTotalPrice().add(newProduct.getPrice()));
--- equalexperts/shoppingcart/src/test/java/com/equalexpert/shoppingcart/ShoppingCartServiceTest.java
+++ equalexperts/shoppingcart/src/test/java/com/equalexpert/shoppingcart/ShoppingCartServiceTest.java
@@ -8,16 +8,49 @@ import java.util.ArrayList;
 
 public class ShoppingCartServiceTest {
 
+    private final ShoppingCartService shoppingCartService = new ShoppingCartService();
+    private final Product doveSoap = new Product("Dove soap", new BigDecimal("39.99"));
+
     @Test
     void given_empty_shopping_cart_when_user_adds_five_product_then_shopping_cart_is_as_expected() {
-        ShoppingCartService shoppingCartService = new ShoppingCartService();
-
         ShoppingCart emptyShoppingCart = new ShoppingCart(new ArrayList<>(), new BigDecimal("0.00"));
-        Product doveSoap = new Product("Dove soap", new BigDecimal("39.99"));
 
         ShoppingCart actualShoppingCart = shoppingCartService.addProduct(emptyShoppingCart, doveSoap, 5);
 
         Assertions.assertEquals(5, actualShoppingCart.getProducts().size());
         Assertions.assertEquals("199.95", actualShoppingCart.getTotalPrice().toString());
+    }
+
+    // edge case scenarios
+
+    @Test
+    void given_null_shopping_cart_when_user_adds_products_throw_exception() {
+        RuntimeException actualException = Assertions.assertThrows(RuntimeException.class,
+            () -> shoppingCartService.addProduct(null, doveSoap, 1));
+
+        Assertions.assertEquals("Error: shopping cart is null", actualException.getMessage());
+    }
+
+    @Test
+    void given_product_list_is_null_when_user_adds_products_throw_exception() {
+        RuntimeException actualException = Assertions.assertThrows(RuntimeException.class,
+            () -> {
+                ShoppingCart shoppingCartWithNullProductList =
+                    new ShoppingCart(null, new BigDecimal("0.00"));
+                shoppingCartService.addProduct(shoppingCartWithNullProductList, doveSoap, 1);
+            });
+
+        Assertions.assertEquals("Error: product list is null", actualException.getMessage());
+    }
+
+    @Test
+    void given_unit_is_zero_or_negative_when_user_adds_products_throw_exception() {
+        RuntimeException actualException = Assertions.assertThrows(RuntimeException.class,
+            () -> {
+                ShoppingCart shoppingCart = new ShoppingCart(new ArrayList<>(), new BigDecimal("0.00"));
+                shoppingCartService.addProduct(shoppingCart, doveSoap, 0);
+            });
+
+        Assertions.assertEquals("Error: units has to be a positive number", actualException.getMessage());
     }
 }
