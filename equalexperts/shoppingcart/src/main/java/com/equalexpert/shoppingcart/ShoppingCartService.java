c73f49e788bde2e3ef53059ee5f8a5d556e36aba
david.crespo <david.crespo@hmcts.net>
2021-12-09 15:33:16 +0000

    add Step1 scenario add unit test and production code refactor code

--- equalexperts/shoppingcart/src/main/java/com/equalexpert/shoppingcart/ShoppingCartService.java
+++ equalexperts/shoppingcart/src/main/java/com/equalexpert/shoppingcart/ShoppingCartService.java
@@ -3,10 +3,19 @@ package com.equalexpert.shoppingcart;
 import java.util.List;
 
 public class ShoppingCartService {
-    public ShoppingCart addProduct(ShoppingCart shoppingCart, Product newProduct) {
+
+    public ShoppingCart addProduct(ShoppingCart shoppingCart, Product newProduct, int units) {
+        if (units == 1) {
+            return addProductOnce(shoppingCart, newProduct);
+        }
+        return addProduct(addProductOnce(shoppingCart, newProduct), newProduct, --units);
+    }
+
+    public ShoppingCart addProductOnce(ShoppingCart shoppingCart, Product newProduct) {
         List<Product> products = shoppingCart.getProducts();
         products.add(newProduct);
         return new ShoppingCart(products, shoppingCart.getTotalPrice().add(newProduct.getPrice()));
     }
 
 }
+
--- equalexperts/shoppingcart/src/test/java/com/equalexpert/shoppingcart/ShoppingCartServiceTest.java
+++ equalexperts/shoppingcart/src/test/java/com/equalexpert/shoppingcart/ShoppingCartServiceTest.java
@@ -9,15 +9,15 @@ import java.util.ArrayList;
 public class ShoppingCartServiceTest {
 
     @Test
-    void given_empty_shopping_cart_when_user_adds_one_product_then_shopping_cart_is_as_expected() {
+    void given_empty_shopping_cart_when_user_adds_five_product_then_shopping_cart_is_as_expected() {
         ShoppingCartService shoppingCartService = new ShoppingCartService();
 
         ShoppingCart emptyShoppingCart = new ShoppingCart(new ArrayList<>(), new BigDecimal("0.00"));
         Product doveSoap = new Product("Dove soap", new BigDecimal("39.99"));
 
-        ShoppingCart actualShoppingCart = shoppingCartService.addProduct(emptyShoppingCart, doveSoap);
+        ShoppingCart actualShoppingCart = shoppingCartService.addProduct(emptyShoppingCart, doveSoap, 5);
 
-        Assertions.assertEquals(1, actualShoppingCart.getProducts().size());
-        Assertions.assertEquals("39.99", actualShoppingCart.getTotalPrice().toString());
+        Assertions.assertEquals(5, actualShoppingCart.getProducts().size());
+        Assertions.assertEquals("199.95", actualShoppingCart.getTotalPrice().toString());
     }
 }
