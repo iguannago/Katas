39b49b219fea025138bf56a2fa670c19bc2104c7
david.crespo <david.crespo@hmcts.net>
2021-12-19 18:02:50 +0000

    raise code coverage to reach 100% coverage

--- equalexperts/shoppingcart/src/test/java/com/equalexpert/shoppingcart/ShoppingCartTest.java
+++ equalexperts/shoppingcart/src/test/java/com/equalexpert/shoppingcart/ShoppingCartTest.java
@@ -8,18 +8,52 @@ import java.util.List;
 
 class ShoppingCartTest {
 
-    private final List<Product> products = List.of(new Product("some product", new BigDecimal("0.00")));
-    private final ShoppingCart shoppingCart = new ShoppingCart(products, new BigDecimal("0.00"));
+    private final Product someProduct = new Product("some product", new BigDecimal("0.00"));
+    private final Product someOtherProduct = new Product("some other product", new BigDecimal("1.00"));
+    private final ShoppingCart shoppingCartWithSomeOtherProduct = new ShoppingCart(
+        List.of(someOtherProduct),
+        new BigDecimal("0.00")
+    );
+
+    private final ShoppingCart shoppingCartWithSomeProduct = new ShoppingCart(
+        List.of(someProduct),
+        new BigDecimal("0.00")
+    );
 
     @Test
     void getProducts() {
-        Assertions.assertEquals(products, shoppingCart.getProducts());
+        Assertions.assertEquals(List.of(someProduct), shoppingCartWithSomeProduct.getProducts());
     }
 
     @Test
     void getTotalPrice() {
-        Assertions.assertEquals(new BigDecimal("0.00"), shoppingCart.getTotalPrice());
+        Assertions.assertEquals(new BigDecimal("0.00"), shoppingCartWithSomeProduct.getTotalPrice());
     }
 
-    //todo: add equals, hashCode and toString tests (MUST)
+    @Test
+    void hashCodeTest() {
+        Assertions.assertEquals(
+            shoppingCartWithSomeProduct.hashCode(),
+            new ShoppingCart(List.of(someProduct), new BigDecimal("0.00")).hashCode()
+        );
+
+        Assertions.assertNotEquals(shoppingCartWithSomeProduct, shoppingCartWithSomeOtherProduct);
+    }
+
+    @Test
+    void equalsTest() {
+        Assertions.assertNotEquals(shoppingCartWithSomeProduct, shoppingCartWithSomeOtherProduct);
+        Assertions.assertEquals(
+            shoppingCartWithSomeProduct,
+            new ShoppingCart(List.of(someProduct), new BigDecimal("0.00"))
+        );
+    }
+
+    @Test
+    void toStringTest() {
+        Assertions.assertEquals(
+            "ShoppingCart{products=[Product{name='some product', price=0.00}], totalPrice=0.00}",
+            shoppingCartWithSomeProduct.toString()
+        );
+    }
 }
