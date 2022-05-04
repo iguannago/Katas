8137765225ad254aec232197a295785ede14d95b
david.crespo <david.crespo@hmcts.net>
2022-05-04 23:15:41 +0100

    add readme and add a few more lines to the main.go program

---
+++ go/README.md
@@ -0,0 +1,15 @@
+# Go lang
+
+## Initialise your application
+```go mod init booking-app```
+
+## Add your go file
+```
+package main
+
+import "fmt"
+
+func main() {...
+```
+## Run your programa
+``` go run main.go```
--- go/main.go
+++ go/main.go
@@ -3,18 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
+	conferenceName := "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	remainingTickets := 50
 
 	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", remainingTickets, conferenceTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
-	//ask user for his name
 
-	userName = "Tom"
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	fmt.Print("Enter your first name: ")
+	fmt.Scan(&firstName)
+	fmt.Print("Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Print("Enter your email: ")
+	fmt.Scan(&email)
+	fmt.Print("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	var bookings [50]string
+	bookings[0] = firstName + " " + lastName
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first element: %v\n", bookings[0])
+
+	fmt.Printf("Thank you %v %v for booking %v tickets.\n", firstName, lastName, userTickets)
+	remainingTickets = conferenceTickets - userTickets
+	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 }
