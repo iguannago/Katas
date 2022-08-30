d1f8c5fd1cd75d36d6cd92c6ac1f2ec9bf0045ff
david.crespo <david.crespo@hmcts.net>
2022-08-30 18:32:52 +0100

    add fizz and buzz scenarios

--- src/main/java/com/katas/fizzbuzz/FizzBuzz27.java
+++ src/main/java/com/katas/fizzbuzz/FizzBuzz27.java
@@ -2,6 +2,16 @@ package com.katas.fizzbuzz;
 
 public class FizzBuzz27 {
     public String play(int number) {
-        return "1";
+        if (isMultipleOf(number, 3)) {
+            return "Fizz";
+        }
+        if (isMultipleOf(number, 5)) {
+            return "Buzz";
+        }
+        return String.valueOf(number);
+    }
+
+    private boolean isMultipleOf(int number, int x) {
+        return number % x == 0;
     }
 }
--- src/test/java/com/katas/fizzbuzz/FizzBuzz27Test.kt
+++ src/test/java/com/katas/fizzbuzz/FizzBuzz27Test.kt
@@ -9,10 +9,16 @@ class FizzBuzz27Test {
     private val fizzBuzz27 = FizzBuzz27()
 
     @ParameterizedTest
-    @CsvSource(value = [
-        "1,1"
-    ])
-    internal fun `given 1 returns 1`(expectedOutcome: String, input: Int) {
+    @CsvSource(
+        value = [
+            "1, 1",
+            "2, 2",
+            "3, Fizz",
+            "4, 4",
+            "5, Buzz"
+        ]
+    )
+    internal fun `given 1 returns 1`(input: Int, expectedOutcome: String) {
         Assertions.assertEquals(expectedOutcome, fizzBuzz27.play(input))
     }
 }
