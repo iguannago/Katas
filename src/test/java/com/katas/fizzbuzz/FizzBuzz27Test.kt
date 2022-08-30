a036945125ccef57f78f68f314a2508beb609258
david.crespo <david.crespo@hmcts.net>
2022-08-30 18:28:58 +0100

    parametrise test

--- src/test/java/com/katas/fizzbuzz/FizzBuzz27Test.kt
+++ src/test/java/com/katas/fizzbuzz/FizzBuzz27Test.kt
@@ -1,14 +1,18 @@
 package com.katas.fizzbuzz
 
 import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class FizzBuzz27Test {
 
-    @Test
-    internal fun `given 1 returns 1`() {
-        val fizzBuzz27 = FizzBuzz27()
+    private val fizzBuzz27 = FizzBuzz27()
 
-        Assertions.assertEquals("1", fizzBuzz27.play(1))
+    @ParameterizedTest
+    @CsvSource(value = [
+        "1,1"
+    ])
+    internal fun `given 1 returns 1`(expectedOutcome: String, input: Int) {
+        Assertions.assertEquals(expectedOutcome, fizzBuzz27.play(input))
     }
 }
