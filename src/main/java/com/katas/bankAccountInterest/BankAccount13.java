6e43612086d122fbc2e6e0a6e0cc7048110cadd4
david.crespo <david.crespo@hmcts.net>
2023-03-28 14:11:20 +0100

    add other ACs.

--- src/main/java/com/katas/bankAccountInterest/BankAccount13.java
+++ src/main/java/com/katas/bankAccountInterest/BankAccount13.java
@@ -4,6 +4,18 @@ import java.math.BigDecimal;
 
 public class BankAccount13 {
 	public BigDecimal getInterest(BigDecimal amount) {
+		if (amount.compareTo(BigDecimal.ZERO) < 0) {
+			throw new IllegalArgumentException("Amount cannot be negative");
+		}
+		if (amount.compareTo(new BigDecimal("3000")) > 0) {
+			return new BigDecimal("1.3");
+		}
+		if (amount.compareTo(new BigDecimal("2000")) > 0) {
+			return new BigDecimal("1.22");
+		}
+		if (amount.compareTo(new BigDecimal("1000")) > 0) {
+			return new BigDecimal("1.15");
+		}
 		return new BigDecimal("1.1");
 	}
 }
--- src/test/java/com/katas/bankAccountInterest/BankAccount13Test.java
+++ src/test/java/com/katas/bankAccountInterest/BankAccount13Test.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /*
  * Exercise: Implement a function to get the right interest given an amount of money.
@@ -16,7 +17,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * Given an amount > 3000 then the interest is 1.3%
  *
  * Also given that:
- * customer wants for the amount to be a BigDecimal
  * customer wants to return an exception if the amount is negative
  */
 public class BankAccount13Test {
@@ -28,8 +28,30 @@ public class BankAccount13Test {
 		assertEquals(new BigDecimal("1.1"), bankAccount13.getInterest(new BigDecimal(amount)));
 	}
 
-	@Test
-	void givenAmount_Then1_15() {
+	@ParameterizedTest
+	@CsvSource({"1001", "1100", "1999", "2000"})
+	void givenAmount_Then1_15(String amount) {
+		BankAccount13 bankAccount13 = new BankAccount13();
+		assertEquals(new BigDecimal("1.15"), bankAccount13.getInterest(new BigDecimal(amount)));
+	}
 
+	@ParameterizedTest
+	@CsvSource({"2001", "2100", "2999", "3000"})
+	void givenAmount_Then1_22(String amount) {
+		BankAccount13 bankAccount13 = new BankAccount13();
+		assertEquals(new BigDecimal("1.22"), bankAccount13.getInterest(new BigDecimal(amount)));
+	}
+
+	@ParameterizedTest
+	@CsvSource({"3001", "3100", "3999", "4000"})
+	void givenAmount_Then1_3(String amount) {
+		BankAccount13 bankAccount13 = new BankAccount13();
+		assertEquals(new BigDecimal("1.3"), bankAccount13.getInterest(new BigDecimal(amount)));
+	}
+
+	@Test
+	void givenNegativeAmount_ThenThrowException() {
+		BankAccount13 bankAccount13 = new BankAccount13();
+		assertThrows(IllegalArgumentException.class, () -> bankAccount13.getInterest(new BigDecimal("-1")));
 	}
 }
